7264b93180f3ff0238022e412e9367cdf059f7d2
andjela-kostic <61754028+andjela-kostic@users.noreply.github.com>
2021-04-23 03:57:21 +0200

    423
    3:57

--- detail.php
+++ detail.php
@@ -24,158 +24,156 @@
 
 <body>
 			<!-- start header -->
-						<div id="header">
-							<div id="menu">
-									<?php
-										include("includes/menu.inc.php");
-									?>
-							</div>
-						</div>
-
-						<div id="logo-wrap">
-							<div id="logo">
-								<?php
-									include("includes/logo.inc.php");
-								?>
-							</div>
-						</div>
-			<!-- end header -->
+	<?php
+		include("includes/menu.inc.php");
+    ?>
+	<header>
+		<div id="logo">
+			<?php
+				include("includes/logo.inc.php");
+			?>
+		</div>
+	</header>
+<!-- end header -->
 		
-			<!-- start page -->
+<!-- start page -->
 
-				<div id="page">
-						<!-- start content -->
-							<div id="content">
-								<div class="post">
-									<h1 class="title"><?php echo $row1['b_nm'];?></h1>
-									<div class="entry">
-										<?php
-										
-											echo '	<table border="0" width="100%">
-												 <tr>
-													<td><hr color="purple"></td>
-												</tr>
-												 <tr align="center" bgcolor="#EEE9F3">
-													 <td>Detalji o proizvodu</td>
-												</tr>
-												<tr>
-													<td><hr color="purple"></td>
-												</tr>
-											 </table>
-											
-											<table border="0"  width="100%" bgcolor="#ffffff">
-												<tr> 
-													
-													<td width="15%" rowspan="3">
-														<img src="'.$row1['b_img'].'" width="100">
-													
-													</td>
-												</tr>
-											
-												<tr> 
-													<td width="50%" height="100%">
-														<table border="0"  width="100%" height="100%">
-															<tr valign="top">
-																<td align="right" width="10%">NAME</td>
-																<td width="6%">: </td>
-																<td align="left">'.$row1['b_nm'].'</td>
-															</tr>
+<div id="page" class="row">					
+	<!-- start sidebar -->
+	<div id="sidebar" class="col-3 bg-dark">
+			<?php
+				include("includes/search.inc.php");
+			?>
+	</div>
+	<!-- end sidebar -->
 
-															
-															<tr>
-																<td align="right">ISBN</td>
-																<td>: </td>
-																<td align="left">'.$row1['b_isbn'].'</td>
-																
-															</tr>
-															
-																					
-															<tr>
-																<td align="right">Publisher </td>
-																<td>: </td>
-																<td align="left">'.$row1['b_publisher'].'</td>
-																
-															</tr>											
-															
-															<tr>
-																<td align="right"> Izdanje </td>
-																<td> : </td>
-																<td align="left">'.$row1['b_edition'].'</td>
-																
-															</tr>
-															
-															<tr>
-																<td align="right">  Broj strana </td>
-																<td> : </td>
-																<td align="left">'.$row1['b_page'].'</td>
-															</tr>
-															
-															<tr>
-																<td align="right"> Cena </td>
-																<td> : </td>
-																<td align="left">'.$row1['b_price'].'</td>
-															</tr>
-														</table>
-										
-														
-													</td>
-												</tr>
-											</table>
-										
-											
-											
-											<table border="0" width="100%">
-												 <tr>
-													<td><hr color="purple"></td>
-												</tr>
-												 <tr align="center" bgcolor="#EEE9F3">
-													 <td>Opis</td>
-												</tr>
-												<tr>
-													<td><hr color="purple"></td>
-												</tr>
-																		
-											 </table>
-											 
-											 '.$row1['b_desc'].'
-																				
 
-											 
-											 <tr><td colspan=2><hr color="purple"></td></tr>
-											
-											<table border="0" width="100%">
-												
-												 <tr align="center" bgcolor="#EEE9F3">';
-												 
-												 if(isset($_SESSION['status']))
-												 {
-													echo ' <td><a href="process_cart.php?nm='.$row1['b_nm'].'&cat='.$_GET['cat'].'&rate='.$row1['b_price'].'">
-														<img src="images/addcart.jpg">
-													</a></td>';
-												}
-												else
-												{
-													echo '<td><img src="images/addcart.jpg"><br><a href="register.php"> <h4>Da biste kupili ovaj proizvod, molimo, ulogujte se.</h4></a></td>';
-												}
-												echo '</tr>
-											</table>';
-										?>
-									</div>
-				
-								</div>
+	<!-- start content -->
+	<div id="content" class="col-9">
+		<div class="post bg-dark">
+			<h1 class="title"><?php echo $row1['b_nm'];?></h1>
+		</div>
+		<div class="entry">
+			<?php
 			
-							</div>
-						<!-- end content -->
-							<!-- start sidebar -->
-							<div id="sidebar">
-									<?php
-										include("includes/search.inc.php");
-									?>
-							</div>
-							<!-- end sidebar -->
-								<div style="clear: both;">&nbsp;</div>
+				echo '	<table border="0" width="100%">
+						<tr>
+						<td><hr color="purple"></td>
+					</tr>
+						<tr align="center" bgcolor="#EEE9F3">
+							<td>Detalji o proizvodu</td>
+					</tr>
+					<tr>
+						<td><hr color="purple"></td>
+					</tr>
+					</table>
 				
-				</div> 
+				<table border="0"  width="100%" bgcolor="#ffffff">
+					<tr> 
+						
+						<td width="15%" rowspan="3">
+							<img src="'.$row1['b_img'].'" width="100">
+						
+						</td>
+					</tr>
+				
+					<tr> 
+						<td width="50%" height="100%">
+							<table border="0"  width="100%" height="100%">
+								<tr valign="top">
+									<td align="right" width="10%">NAME</td>
+									<td width="6%">: </td>
+									<td align="left">'.$row1['b_nm'].'</td>
+								</tr>
+
+								
+								<tr>
+									<td align="right">ISBN</td>
+									<td>: </td>
+									<td align="left">'.$row1['b_isbn'].'</td>
+									
+								</tr>
+								
+														
+								<tr>
+									<td align="right">Publisher </td>
+									<td>: </td>
+									<td align="left">'.$row1['b_publisher'].'</td>
+									
+								</tr>											
+								
+								<tr>
+									<td align="right"> Izdanje </td>
+									<td> : </td>
+									<td align="left">'.$row1['b_edition'].'</td>
+									
+								</tr>
+								
+								<tr>
+									<td align="right">  Broj strana </td>
+									<td> : </td>
+									<td align="left">'.$row1['b_page'].'</td>
+								</tr>
+								
+								<tr>
+									<td align="right"> Cena </td>
+									<td> : </td>
+									<td align="left">'.$row1['b_price'].'</td>
+								</tr>
+							</table>
+			
+							
+						</td>
+					</tr>
+				</table>
+			
+				
+				
+				<table border="0" width="100%">
+						<tr>
+						<td><hr color="purple"></td>
+					</tr>
+						<tr align="center" bgcolor="#EEE9F3">
+							<td>Opis</td>
+					</tr>
+					<tr>
+						<td><hr color="purple"></td>
+					</tr>
+											
+					</table>
+					
+					'.$row1['b_desc'].'
+													
+
+					
+					<tr><td colspan=2><hr color="purple"></td></tr>
+				
+				<table border="0" width="100%">
+					
+						<tr align="center" bgcolor="#EEE9F3">';
+						
+						if(isset($_SESSION['status']))
+						{
+						echo ' <td><a href="process_cart.php?nm='.$row1['b_nm'].'&cat='.$_GET['cat'].'&rate='.$row1['b_price'].'">
+							<img src="images/addcart.jpg">
+						</a></td>';
+					}
+					else
+					{
+						echo '<td><img src="images/addcart.jpg"><br><a href="register.php"> <h4>Da biste kupili ovaj proizvod, molimo, ulogujte se.</h4></a></td>';
+					}
+					echo '</tr>
+				</table>';
+			?>
+		</div>
+
+		</div>
+
+	</div>
+<!-- end content -->
+							
+				
+
 			<!-- end page -->
 						<!-- start footer -->
 						<div id="footer">
--- search_result.php
+++ search_result.php
@@ -20,23 +20,18 @@ require('includes/config.php');
 </head>
 
 <body>
-			<!-- start header -->
-				<div id="header">
-					<div id="menu">
-						<?php
-							include("includes/menu.inc.php");
-						?>
-					</div>
-				</div>
-				
-				<div id="logo-wrap">
-					<div id="logo">
-							<?php
-								include("includes/logo.inc.php");
-							?>
-					</div>
-				</div>
-			<!-- end header -->
+<!-- start header -->
+	<?php
+		include("includes/menu.inc.php");
+    ?>
+	<header>
+		<div id="logo">
+			<?php
+				include("includes/logo.inc.php");
+			?>
+		</div>
+	</header>
+<!-- end header -->
 			
 			<!-- start page -->
 
@@ -56,33 +51,33 @@ require('includes/config.php');
 	<div id="content" class="col-9">
 		<div class="post bg-dark">
 			<h1 class="title"><?php echo @$_GET['cat'];?></h1>
-			<div class="entry">				
-				<table border="3" width="100%" >
-					<?php
-						$count=0;
-						while($row=mysqli_fetch_assoc($res1))
+		</div>
+		<div class="entry">				
+			<table border="3" width="100%" >
+				<?php
+					$count=0;
+					while($row=mysqli_fetch_assoc($res1))
+					{
+						if($count==0)
 						{
-							if($count==0)
-							{
-								echo '<tr>';
-							}
-							
-							echo '<td valign="top" width="20%" align="center">
-								<a href="detail.php?id='.$row['b_id'].'">
-								<img src="'.$row['b_img'].'" width="80" height="100">
-								<br>'.$row['b_nm'].'</a>
-							</td>';
-							$count++;							
-							
-							if($count==4)
-							{
-								echo '</tr>';
-								$count=0;
-							}
+							echo '<tr>';
 						}
-					?>
-				</table>
-			</div>									
+						
+						echo '<td valign="top" width="20%" align="center">
+							<a href="detail.php?id='.$row['b_id'].'">
+							<img src="'.$row['b_img'].'" width="80" height="100">
+							<br>'.$row['b_nm'].'</a>
+						</td>';
+						$count++;							
+						
+						if($count==4)
+						{
+							echo '</tr>';
+							$count=0;
+						}
+					}
+				?>
+			</table>								
 		</div>								
 	</div>
 </div>
